778408e4676d09060188d124d93f68fe077dd0c9
24DP4RSeks <A240794RS@rvt.lv>
2025-10-23 11:19:01 +0300

    start 3-2 java

---
+++ src/main/java/rvt/java3_2.java
@@ -0,0 +1,15 @@
+package rvt;
+import java.util.Scanner;
+public class java3_2 {
+    public static void main(String[] args) {
+        
+    }
+    public static void onlyTheseNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int[] list = {72, 2, 8, 11, -1};
+        System.out.print("From where? ");
+        
+
+    }
+}
+
