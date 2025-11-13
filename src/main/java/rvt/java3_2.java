0a41d068ce8f34ef82f669541d8ca00b8c40a064
24DP4RSeks <A240794RS@rvt.lv>
2025-11-13 11:20:14 +0200

    3_2onlythesenumbers

--- src/main/java/rvt/java3_2.java
+++ src/main/java/rvt/java3_2.java
@@ -2,15 +2,22 @@ package rvt;
 import java.util.Scanner;
 public class java3_2 {
     public static void main(String[] args) {
-        
+        onlyTheseNumbers();
     }
     public static void onlyTheseNumbers() {
         Scanner scanner = new Scanner(System.in);
 
         int[] list = {72, 2, 8, 11, -1};
         System.out.print("From where? ");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.print("To where?: ");
+        int second = Integer.valueOf(scanner.nextLine());
+        for(int i = first; i <= second; i++){
+            System.out.println(list[i]);
+        }
+    }
+    public static void listSize() {
         
-
     }
 }
 
