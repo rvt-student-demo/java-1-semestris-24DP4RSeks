62026c90a7f02d555347ef526ea318f02aa6cfec
24DP4RSeks <A240794RS@rvt.lv>
2025-11-28 09:13:58 +0200

    JMArreyList

--- src/main/java/rvt/JMArreyList1.java
+++ src/main/java/rvt/JMArreyList1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class JMArreyList {
+public class JMArreyList1 {
     public static void main(String[] args) {
         ArrayList<String> strings = new ArrayList<>();
 
