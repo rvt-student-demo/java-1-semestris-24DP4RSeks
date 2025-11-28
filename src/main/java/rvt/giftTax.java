3f277c0445e4cfb61aaf8047cc08afcefe5062d5
24DP4RSeks <A240794RS@rvt.lv>
2025-11-28 08:59:45 +0200

    GiftTax

--- src/main/java/rvt/giftTax.java
+++ src/main/java/rvt/giftTax.java
@@ -2,7 +2,7 @@ package rvt;
 import java.util.Scanner;
 
 
-public class giftTax {
+public class GiftTax {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
